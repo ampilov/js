9112219212341649b5f4bf3505242470959b0a06
Anton <anton_ampilov@mail.ru>
2019-10-30 00:54:35 +0300

    add varibles money and time create object appData and exspenses

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+let money = prompt('Ваш бюджет в месяц?','1000' );
+let time = prompt('Введите дату в формате YYYY-MM-DD','2019-10-30' );
 
+let appData = {
+    budget: money,
+    timeData: time,
+
+};
+
+let exp = prompt('Введите обязательную статью расходов в этом месяце','30000');
+let optExp = prompt('Во сколько обойдется?','30000');
+
+let expenses = {
+exp: optExp
+};
+
+let optionalExpenses = {
+
+};
+
+let income = [];
+
+let savings = false;
+
+
+console.log(appData);
+console.log(expenses);
+console.log(appData);
+
+alert('Ваш дневной бюджет равен '+appData.budget/30+' Рублей');
