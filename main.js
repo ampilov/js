59c0c58d58a6cd15fa74691eaef2a3d44f827a0c
Anton <anton_ampilov@mail.ru>
2019-10-30 01:19:04 +0300

    correct first practice task

--- main.js
+++ main.js
@@ -4,27 +4,17 @@ let time = prompt('Введите дату в формате YYYY-MM-DD','2019-1
 let appData = {
     budget: money,
     timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
 
 };
 
-let exp = prompt('Введите обязательную статью расходов в этом месяце','30000');
-let optExp = prompt('Во сколько обойдется?','30000');
+let firstQuestion = prompt('Введите обязательную статью расходов в этом месяце','30000');
+let secondQuestion = prompt('Во сколько обойдется?','30000');
+let thirdQuestion = prompt('Введите обязательную статью расходов в этом месяце','30000');
+let fourthQuestion = prompt('Во сколько обойдется?','30000');
 
-let expenses = {
-exp: optExp
-};
-
-let optionalExpenses = {
-
-};
-
-let income = [];
-
-let savings = false;
-
-
-console.log(appData);
-console.log(expenses);
-console.log(appData);
 
 alert('Ваш дневной бюджет равен '+appData.budget/30+' Рублей');
